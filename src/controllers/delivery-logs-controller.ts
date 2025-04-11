cc4070ec311154601c133b15a21b264782a1dcde
Rafael <rafaelbthomaz@gmail.com>
2025-04-11 15:44:03 -0300

    fix: show delivery not found

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -54,6 +54,10 @@ class DeliveryLogsController {
       },
     })
 
+    if(!delivery){
+      return response.status(404).json({ message: "delivery not found "})
+    }
+
     if(request.user?.role === "customer" && request.user.id !== delivery?.userId) {
       throw new AppError("the user can only view their deliveries", 401)
     }
